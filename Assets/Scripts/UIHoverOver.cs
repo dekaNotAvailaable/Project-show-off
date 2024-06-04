80b8fd99444ce752f146136c6f4fbe1e501f033a
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-06-04 15:52:22 +0200

    wait

--- Assets/Scripts/UIHoverOver.cs
+++ Assets/Scripts/UIHoverOver.cs
@@ -2,7 +2,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-// Make sure your class derives from the correct base class
 public class UIHoverOver : MonoBehaviour
 {
     public Transform leftControllerTransform;
@@ -10,52 +9,51 @@ public class UIHoverOver : MonoBehaviour
     public LayerMask targetCanvas;
     public TMP_Text texts;
     public Image gunImage;
+
     [SerializeField]
     private Vector3 imageRaiseCap = new Vector3(0, 10f, 0);
+
+    private Vector3 initialPosition;
+    private const float hoverSpeed = 0.4f;
+
     private void Start()
     {
         texts.enabled = false;
-        Debug.Log("UIHoverOver script");
+        initialPosition = gunImage.transform.position;
     }
+
     private void Update()
     {
         CheckController(leftControllerTransform);
         CheckController(rightControllerTransform);
     }
 
-    void CheckController(Transform controllerTransform)
+    private void CheckController(Transform controllerTransform)
     {
         RaycastHit hit;
         // Cast a ray from the controller forward
-        if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit, Mathf.Infinity, targetCanvas))
         {
-            Debug.Log("casting a ray");
-            // Check if the raycast hit the canvas
-            if (hit.collider != null)
-            {
-              //  if (hit.collider.gameObject.CompareTag("Canvas"))
-              //  {
-                    Debug.Log("raycast is hitting canvas");
-                    OnHoverCanvas();
-              //  }
-              //  else { Debug.Log("failed to hit canvas collider"); }
-            }
-            else { Debug.Log("collider null"); }
-
+            Debug.Log("Raycast hit detected");
+            OnHoverCanvas();
         }
         else
         {
-            //Debug.Log("failed to cast a ray");
             // Draw a red ray in the Scene view to visualize when the raycast fails
             Debug.DrawRay(controllerTransform.position, controllerTransform.forward * 10, Color.red, 1.0f);
         }
     }
+
     private void OnHoverCanvas()
     {
-        if (gunImage.transform.position.y < gunImage.transform.position.y + imageRaiseCap.y)
+        if (gunImage.transform.position.y < initialPosition.y + imageRaiseCap.y)
         {
-            gunImage.transform.position += new Vector3(0, 1, 0);
+            gunImage.transform.position += new Vector3(0, hoverSpeed * Time.deltaTime, 0);
             texts.enabled = true;
+        }
+        else
+        {
+            gunImage.transform.position = initialPosition + new Vector3(0, imageRaiseCap.y, 0);
         }
     }
 }
