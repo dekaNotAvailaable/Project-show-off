a869466951feadd2eb52ce79e465ea07240b886b
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-06-04 17:02:17 +0200

    dde

--- Assets/Scripts/UIHoverOver.cs
+++ Assets/Scripts/UIHoverOver.cs
@@ -2,59 +2,72 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Make sure your class derives from the correct base class
 public class UIHoverOver : MonoBehaviour
 {
     public Transform leftControllerTransform;
     public Transform rightControllerTransform;
     public LayerMask targetCanvas;
     public TMP_Text texts;
+    public GameObject canvas;
     public Image gunImage;
-
     [SerializeField]
     private Vector3 imageRaiseCap = new Vector3(0, 10f, 0);
-
-    private Vector3 initialPosition;
+    private Vector3 initialPosition = Vector3.zero;
     [SerializeField]
-    private const float hoverSpeed = 0.4f;
-
+    private float riseSpeed = 0.4f;
     private void Start()
     {
+
         texts.enabled = false;
         initialPosition = gunImage.transform.position;
     }
-
     private void Update()
     {
         CheckController(leftControllerTransform);
         CheckController(rightControllerTransform);
     }
 
-    private void CheckController(Transform controllerTransform)
+    void CheckController(Transform controllerTransform)
     {
         RaycastHit hit;
-        // Cast a ray from the controller forward
-        if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit, Mathf.Infinity, targetCanvas))
+        if (canvas.activeSelf)
         {
-            Debug.Log("Raycast hit detected");
-            OnHoverCanvas();
-        }
-        else
-        {
-            // Draw a red ray in the Scene view to visualize when the raycast fails
-            Debug.DrawRay(controllerTransform.position, controllerTransform.forward * 10, Color.red, 1.0f);
+            if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log("casting a ray");
+                if (hit.collider != null)
+                {
+                    // Debug.Log("collider not null");
+                    // if (hit.collider.gameObject.CompareTag("Canvas"))
+                    //{
+                    Debug.Log("raycast is hitting canvas");
+                    OnHoverCanvas();
+                    //}
+                }
+                else { Debug.Log("collider null"); }
+
+            }
+            else
+            {
+                Debug.DrawRay(controllerTransform.position, controllerTransform.forward * 10, Color.red, 1.0f);
+            }
         }
     }
-
     private void OnHoverCanvas()
     {
         if (gunImage.transform.position.y < initialPosition.y + imageRaiseCap.y)
         {
-            gunImage.transform.position += new Vector3(0, hoverSpeed * Time.deltaTime, 0);
+
+            gunImage.transform.position += new Vector3(0, riseSpeed, 0) * Time.deltaTime;
             texts.enabled = true;
+            Debug.Log("go up pictue");
+
         }
         else
         {
-            gunImage.transform.position = initialPosition + new Vector3(0, imageRaiseCap.y, 0);
+            Debug.Log("no go up pictue");
+            gunImage.transform.position += Vector3.zero;
         }
     }
 }
