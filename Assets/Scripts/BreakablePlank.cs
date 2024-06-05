53a29fbd3e668b0f586d9afbdf532efefcad29df
roanweller <roanweller10@gmail.com>
2024-06-05 14:07:53 +0200

    added sounds (not added to objects yet, just put in the files)

--- Assets/Scripts/BreakablePlank.cs
+++ Assets/Scripts/BreakablePlank.cs
@@ -4,12 +4,16 @@ public class BreakablePlank : MonoBehaviour
 {
     [SerializeField]
     private float breakSec = 0;
+
+    [SerializeField] AudioSource myAudio;
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject, breakSec);
             Debug.Log("Breaking Plank");
+            myAudio.Play(0);
         }
     }
 }
