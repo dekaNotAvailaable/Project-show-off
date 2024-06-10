bac9deb28cfd8172815d7e52e5b15759a3b42f66
roanweller <roanweller10@gmail.com>
2024-06-10 13:37:51 +0200

    okay

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,22 +1,25 @@
 
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
+    public AudioSource BGM;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
+    private void Awake() {
+        if (instance != null) {
             Debug.LogError("Found more than 1 Audio manager in the scene.");
+        
         }
+        instance = this;
     }
-
+    private void Start()
+    {
+        PlayBGM();
+    }
+    private void PlayBGM() { BGM.Play(); BGM.loop = true; }
 
 }
