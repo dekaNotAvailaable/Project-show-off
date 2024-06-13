31aa3efb7f71ffc8526534047025dd0d11f00828
Rares Orza <rares.orza@gmail.com>
2024-06-13 16:09:31 +0200

    UI Functionality Finished

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -25,17 +25,18 @@ public class GameMenuManager : MonoBehaviour
         //        menu.transform.forward *= -1;
         //    }
         //}
-        for(int i = 0; i < gameObjects.Length; i++)
+        if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            gameObjects[i].transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-            gameObjects[i].transform.LookAt(new Vector3(head.position.x, gameObjects[i].transform.position.y, head.position.z));
-            gameObjects[i].transform.forward *= -1;
-        }
-        if(SceneManager.GetActiveScene().buildIndex != 0)
-        {
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                gameObjects[i].transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+                gameObjects[i].transform.LookAt(new Vector3(head.position.x, gameObjects[i].transform.position.y, head.position.z));
+                gameObjects[i].transform.forward *= -1;
+            }
             if (showButton.action.WasPressedThisFrame())
             {
                 gameObjects[0].SetActive(!gameObjects[0].activeSelf);
+
             }
         }
     }
