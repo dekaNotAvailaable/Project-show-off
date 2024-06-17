1f4db638a0f8e6ba1ae4d0d40b9b1b803429f133
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-06-17 15:55:37 +0200

    sound

--- Assets/Scripts/AudioCollision.cs
+++ Assets/Scripts/AudioCollision.cs
@@ -1,13 +1,42 @@
 using UnityEngine;
-using System.Collections;
- 
-public class AudioCollision : MonoBehaviour {
+
+public class AudioCollision : MonoBehaviour
+{
     public float magnitude = 20.0f;
-    void OnCollisionEnter(Collision collision) {
- 
+
+    void OnCollisionEnter(Collision collision)
+    {
         if (collision.relativeVelocity.magnitude > magnitude)
+        {
             GetComponent<AudioSource>().Play();
-       
+        }
+
         Debug.Log(collision.collider.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the current GameObject has a specific tag (e.g., "GlassPlank")
+        if (gameObject.CompareTag("GlassPlank"))
+        {
+            // Check if the other collider is tagged as "Player"
+            if (other.CompareTag("Player"))
+            {
+                // Play random footstep sound
+                int random = Random.Range(0, 3);
+                if (random == 0)
+                {
+                    AudioManager.instance.GlassFoot1Play();
+                }
+                else if (random == 1)
+                {
+                    AudioManager.instance.GlassFoot2Play();
+                }
+                else
+                {
+                    AudioManager.instance.GlassFoot3Play();
+                }
+            }
+        }
     }
 }
