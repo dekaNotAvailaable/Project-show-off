9778cc0b1d9c18027ab6307270a1f473ad393e9b
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-06-18 21:39:22 +0200

    v1

---
+++ Assets/Scripts/Audio/GlassFootStepsAudio.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GlassFootStepsAudio : MonoBehaviour
+{
+    private AudioSource[] glassFootSteps;
+    private void Start()
+    {
+        glassFootSteps = GetComponents<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            PlayRandomAudio();
+        }
+    }
+    private void PlayRandomAudio()
+    {
+        if (glassFootSteps.Length == 0)
+        {
+            Debug.LogWarning("No AudioSources found!");
+            return;
+        }
+        int randomIndex = Random.Range(0, glassFootSteps.Length);
+        glassFootSteps[randomIndex].Play();
+    }
+}
