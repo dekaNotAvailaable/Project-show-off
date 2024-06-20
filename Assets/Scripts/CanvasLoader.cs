76c6f89ce984407645ca17700548ebba262b0e79
elia-project <131658240+elia-project@users.noreply.github.com>
2024-06-20 13:14:56 +0200

    UI Win panel - Script modification Elia

--- Assets/Scripts/CanvasLoader.cs
+++ Assets/Scripts/CanvasLoader.cs
@@ -8,8 +8,15 @@ public class CanvasLoader : MonoBehaviour
     public GameObject EndMenu;
     public GameObject player;
     public Transform head;
+    public Animator animator;//elia
     private void OnTriggerEnter(Collider collision)
     {
+        //Temporarily disable animation
+        if (animator != null)
+        {
+            animator.enabled = false;
+        }
+
         if (collision.gameObject.CompareTag("Player"))
         {
             EndMenu.SetActive(true);
@@ -18,6 +25,12 @@ public class CanvasLoader : MonoBehaviour
             EndMenu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * 2;
             EndMenu.transform.LookAt(new Vector3(head.position.x, EndMenu.transform.position.y, head.position.z));
             EndMenu.transform.forward *= -1;
+        }
+
+        //Rehabilitates animation
+        if (animator != null)
+        {
+            animator.enabled = true;
         }
     }
 }
