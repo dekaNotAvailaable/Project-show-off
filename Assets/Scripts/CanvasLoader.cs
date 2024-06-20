28a1e063f1195a613903d78c4b4648d88e592c43
roanweller <roanweller10@gmail.com>
2024-06-20 14:34:15 +0200

    Update CanvasLoader.cs

--- Assets/Scripts/CanvasLoader.cs
+++ Assets/Scripts/CanvasLoader.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -32,6 +32,37 @@ public class CanvasLoader : MonoBehaviour
         if (animator != null)
         {
             animator.enabled = true;
+        }
+    }
+}*/
+using UnityEngine;
+
+namespace UI
+{
+    public class CanvasLoader : MonoBehaviour
+    {
+        [SerializeField] private Transform playerHead;
+        [SerializeField] private float spawnDistance = 2f;
+        [SerializeField] private float yOffset = 0f;
+        
+        [SerializeField] private GameObject panel;
+        
+        private Vector3 _playerHeadForward;
+
+        private void Awake()
+        {
+            _playerHeadForward = playerHead.forward;
+        }
+
+        private void Update()
+        {          
+            // show the panel in front of the player
+            var position = playerHead.position;
+            panel.transform.position = position + new Vector3(_playerHeadForward.x, yOffset, _playerHeadForward.z).normalized * spawnDistance;
+            
+            // rotate the panel to face the player frame by frame
+            panel.transform.LookAt(new Vector3(position.x, panel.transform.position.y, position.z));
+            panel.transform.forward *= -1;
         }
     }
 }
